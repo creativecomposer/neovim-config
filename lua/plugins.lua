3ac4384d180c1f450933428397180cbc683d927f
Creative Composer <rtlearner12@gmail.com>
2024-01-14 15:31:42 +0000

    Update telescope.nvim install version

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,7 +11,7 @@ return require('packer').startup(function(use)
   })
 
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1', -- or, branch = '0.1.x',
+    'nvim-telescope/telescope.nvim', tag = '0.1.5', -- or, branch = '0.1.x',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
