ab11db709c058076278b30f553924072b635a929
Creative Composer <rtlearner12@gmail.com>
2024-12-26 19:26:55 +0000

    Prefer git clone install for treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,4 +1,6 @@
-require'nvim-treesitter.configs'.setup {
+require("nvim-treesitter.install").prefer_git = true
+
+require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "javascript", "typescript", "python", "c", "lua", "vim", "vimdoc", "query" },
 
